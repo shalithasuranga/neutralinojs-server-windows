328be1f8f9b3ffbff0aa9e1c85bd92d5a6f2c104
Shalitha Suranga <shalithasuranga@gmail.com>
2018-04-10 01:12:31 +0530

    Add JSON test content

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include "serverlistener.h"
 #include <windows.h>
+#include "../lib/json/json.hpp"
+
+using json = nlohmann::json;
 
 int main() {
+    json j;
+
+    // add a number that is stored as double (note the implicit conversion of j to an object)
+    j["pi"] = {"test", "true"};
+
+    std::cout << j << std::endl;
     ServerListener().run();
     return 0;
 }
