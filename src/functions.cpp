3caa30e565e2d175b8de0ddb0e1a55405943ba95
Shalitha Suranga <shalithasuranga@gmail.com>
2018-04-11 17:09:05 +0530

    Add global functions file

---
+++ src/functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
